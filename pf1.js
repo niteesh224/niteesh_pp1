39b3210658d437f8bab3fca598dc127357640e20
niteesh <niteesh224@gmail.com>
2024-10-13 10:25:31 +0530

    Added CORS to allow frontend access

--- pf1.js
+++ pf1.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require("cors")
 
 const app = express();
 const port = process.env.PORT || 3000;
